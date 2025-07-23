9f052c51f3776936de79629bc167b42aec774bc5
Fernando Nagase <nagase.fernando@gmail.com>
2025-07-22 21:53:11 -0300

    Adicionar rota /tenant

---
+++ app_server/db.js
@@ -0,0 +1,13 @@
+const db = {
+    teste: {
+        api: 'teste.example.com',
+    },
+    batata: {
+        api: 'batata.example.com',
+    },
+    banana: {
+        api: 'banana.example.com',
+    },
+}
+
+module.exports = db;
--- app_server/index.js
+++ app_server/index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const db = require('./db.js');
+
+app.get('/tenant', (req, res) => {
+  const tenant = req.headers['x-tenant-id'];
+  if (db[tenant]) {
+    res.json(db[tenant]);
+  } else {
+    res.status(404).send('Tenant not found');
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
