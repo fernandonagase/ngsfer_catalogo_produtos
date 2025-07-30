f6caba8978e02ea97529f1a55faa009d7b3aaa7d
Fernando Nagase <nagase.fernando@gmail.com>
2025-07-29 21:46:09 -0300

    Adicionar rota de GET de produto

--- app_server/index.js
+++ app_server/index.js
@@ -18,6 +18,17 @@ app.get('/products', (req, res) => {
   res.json(products);
 });
 
+app.get('/products/:id', (req, res) => {
+  const productId = parseInt(req.params.id, 10);
+  const product = products.find(p => p.id === productId);
+  
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Produto não encontrado' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
