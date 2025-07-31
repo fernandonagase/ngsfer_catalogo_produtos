21128276aa7b34b583e6390f57eaea5f4e2d5c28
Fernando Nagase <nagase.fernando@gmail.com>
2025-07-30 22:08:12 -0300

    Adicionar dados do tenant no tenant_server

--- app_tenant_server/db.js
+++ app_tenant_server/db.js
@@ -2,14 +2,23 @@ const db = {
     teste: {
         subdomain: 'teste',
         api: 'http://teste.example.com:3001',
+        nomeLoja: 'Teste Store',
+        descricaoLoja: 'Loja de Teste',
+        urlLogoLoja: 'https://placedog.net/300/300',
     },
     batata: {
         subdomain: 'batata',
         api: 'http://batata.example.com:3001',
+        nomeLoja: 'Batata Store',
+        descricaoLoja: 'A sua loja de batatas!',
+        urlLogoLoja: 'https://placecats.com/300/300',
     },
     banana: {
         subdomain: 'banana',
         api: 'http://banana.example.com:3001',
+        nomeLoja: 'Banana Store',
+        descricaoLoja: 'A sua loja de bananas!',
+        urlLogoLoja: 'https://picsum.dev/300/300',
     },
 }
 
--- app_tenant_server/src/dtos/tenant-dto.js
+++ app_tenant_server/src/dtos/tenant-dto.js
@@ -1,10 +1,16 @@
 class TenantDto {
     subdomain;
     api;
+    nomeLoja;
+    descricaoLoja;
+    urlLogoLoja = 'https://picsum.dev/300/300';
 
     constructor(tenant) {
         this.subdomain = tenant.subdomain;
         this.api = tenant.api;
+        this.nomeLoja = tenant.nomeLoja;
+        this.descricaoLoja = tenant.descricaoLoja;
+        this.urlLogoLoja = tenant.urlLogoLoja || this.urlLogoLoja;
     }
 }
 
