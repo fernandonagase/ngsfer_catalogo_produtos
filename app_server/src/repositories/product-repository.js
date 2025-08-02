ce7b02e0ed443d73b4191314b3c404a0a21b0c4d
Fernando Nagase <nagase.fernando@gmail.com>
2025-08-02 20:38:21 -0300

    Criar repositório de produtos

---
+++ app_server/src/repositories/product-repository.js
@@ -0,0 +1,11 @@
+const products = [
+  { id: 1, name: "Produto 1", price: 100 },
+  { id: 2, name: "Produto 2", price: 200 },
+  { id: 3, name: "Produto 3", price: 300 },
+];
+
+function getAllProducts() {
+  return [...products];
+}
+
+module.exports = { getAllProducts };
