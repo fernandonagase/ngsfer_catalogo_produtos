9acb98478d9bca332bc17327d600dd409a5238ec
Fernando Nagase <nagase.fernando@gmail.com>
2025-08-05 22:57:02 -0300

    Melhorar layout da página principal

--- app_ui/src/pages/IndexPage.vue
+++ app_ui/src/pages/IndexPage.vue
@@ -1,16 +1,17 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="row q-col-gutter-md container">
+  <q-page padding class="flex flex-center">
+    <div class="row q-col-gutter-md layout-container">
       <div v-for="product in productStore.products" :key="product.id" class="col-xs-12 col-sm-4">
         <q-card>
           <q-card-section>
-            <div class="text-h6">{{ product.name }}</div>
-            <div class="text-subtitle2">R$ {{ product.price }}</div>
+            <div class="text-h6">{{ product.nome }}</div>
+            <div class="text-subtitle2">{{ formatCurrencyFromCents(product.preco) }}</div>
           </q-card-section>
           <q-card-actions>
             <q-btn
               color="primary"
               label="Ver detalhes"
+              unelevated
               :to="{ name: 'produto-detalhes', params: { productId: product.id } }"
             />
           </q-card-actions>
@@ -26,6 +27,8 @@ import { useMeta } from 'quasar'
 
 import { useTenantStore } from 'src/stores/tenant-store'
 import { useProductStore } from 'src/stores/product-store'
+
+import { formatCurrencyFromCents } from 'src/helpers/currency.js';
 
 export default defineComponent({
   name: 'IndexPage',
@@ -52,13 +55,8 @@ export default defineComponent({
       productStore,
       fetchAllProducts,
       tenantIdentifier,
+      formatCurrencyFromCents,
     }
   },
 })
 </script>
-
-<style lang="scss" scoped>
-.container {
-  width: min(80%, 800px);
-}
-</style>
