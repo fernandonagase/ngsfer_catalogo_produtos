d05640f739e559446c43d81ac9e40cc40de26d11
Fernando Nagase <nagase.fernando@gmail.com>
2025-08-05 23:12:03 -0300

    Melhorar layout da tela de detalhe de produto

--- app_ui/src/pages/ProductPage.vue
+++ app_ui/src/pages/ProductPage.vue
@@ -1,13 +1,24 @@
 <template>
-    {{ produto.id }}
-    {{ produto.name }}
-    {{ produto.price }}
+  <q-page padding>
+      <div class="layout-container q-mx-auto">
+        <h1 class="text-h4 q-mb-sm">{{ produto.nome }}</h1>
+        <p v-if="produto.marca">Marca: {{ produto.marca.nome }}</p>
+        <p class="text-subtitle1 text-weight-bold">{{ formatCurrencyFromCents(produto.preco) }}</p>
+        <h2 class="text-h5 q-mb-xs">Sobre o produto</h2>
+        <p class="text-body1">{{ produto.descricao }}</p>
+        <h3 class="text-h6 q-mb-sm">Categorias</h3>
+        <div class="q-gutter-sm">
+          <span v-for="categoria in produto.categorias" :key="categoria.id">{{ categoria.nome }}</span>
+        </div>
+      </div>
+  </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 import { useProductStore } from 'src/stores/product-store';
+import { formatCurrencyFromCents } from 'src/helpers/currency.js';
 
 export default defineComponent({
     name: 'ProductPage',
@@ -25,7 +36,8 @@ export default defineComponent({
         const productStore = useProductStore();
 
         return {
-            produto: productStore.product
+            produto: productStore.product,
+            formatCurrencyFromCents,
         };
     }
 })
