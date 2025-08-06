bd7a65c7a2d4d1b6a59ea2b5b76535cf0fa89e9f
Fernando Nagase <nagase.fernando@gmail.com>
2025-08-05 22:44:13 -0300

    Ajustar rotas da API

--- app_ui/src/stores/product-store.js
+++ app_ui/src/stores/product-store.js
@@ -8,7 +8,7 @@ export const useProductStore = defineStore('productStore', {
   actions: {
     async fetchAllProducts() {
       try {
-        const ret = await this.$api.get('/products')
+        const ret = await this.$api.get('/api/v1/produtos')
         this.products = ret.data
       } catch (error) {
         console.error('Error fetching products:', error)
@@ -16,7 +16,7 @@ export const useProductStore = defineStore('productStore', {
     },
     async fetchById(productId) {
       try {
-        const ret = await this.$api.get(`/products/${productId}`)
+        const ret = await this.$api.get(`/api/v1/produtos/${productId}`)
         this.product = ret.data
       } catch (error) {
         console.error(`Error fetching product with ID ${productId}:`, error)
