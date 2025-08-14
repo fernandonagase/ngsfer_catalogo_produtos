e8c4e51e7f2897c87e23a6661a6e5bf08cdbaee9
Fernando Nagase <nagase.fernando@gmail.com>
2025-08-13 22:02:10 -0300

    Ajustar rota de produtos

--- app_ui/src/router/routes.js
+++ app_ui/src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [
     children: [
       { path: '', name: 'produto-lista', component: () => import('pages/IndexPage.vue') },
       {
-        path: '/products/:productSlug',
+        path: '/produtos/:productSlug',
         name: 'produto-detalhes',
         component: () => import('pages/ProductPage.vue'),
         props: true,
