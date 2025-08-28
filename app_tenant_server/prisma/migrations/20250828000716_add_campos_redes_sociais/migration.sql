b829a2eb7f204456e992e7912ce994e28ff21b5e
Fernando Nagase <nagase.fernando@gmail.com>
2025-08-27 21:10:42 -0300

    Adicionar campos de redes sociais

---
+++ app_tenant_server/prisma/migrations/20250828000716_add_campos_redes_sociais/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "public"."empresa" ADD COLUMN     "facebook" VARCHAR(100),
+ADD COLUMN     "instagram" VARCHAR(100),
+ADD COLUMN     "linkedin" VARCHAR(100),
+ADD COLUMN     "tiktok" VARCHAR(100),
+ADD COLUMN     "twitter" VARCHAR(100),
+ADD COLUMN     "youtube" VARCHAR(100);
