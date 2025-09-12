19e69148b6ed00030768c2019c87aa7e2101f3e5
Fernando Nagase <nagase.fernando@gmail.com>
2025-09-11 21:14:35 -0300

    Extrair componente ProductImageViewer

---
+++ app_ui/src/components/ProductImageViewer.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  images: {
+    type: Array,
+    required: true,
+  },
+})
+
+const selectedImage = ref(
+  (props.images.find((image) => image.position === 1) || props.images[0]) ?? {
+    url: 'https://placedog.net/1024/768',
+    altText: '',
+  },
+)
+</script>
+
+<template>
+  <div>
+    <q-img
+      :src="selectedImage.url"
+      :alt="selectedImage.altText"
+      :ratio="16 / 9"
+      spinner-color="primary"
+      spinner-size="82px"
+    />
+  </div>
+</template>
--- app_ui/src/pages/ProductPage.vue
+++ app_ui/src/pages/ProductPage.vue
@@ -3,13 +3,7 @@
     <div class="layout-container q-mx-auto">
       <div class="row q-col-gutter-lg q-mb-xl">
         <div class="col-xs-12 col-sm-6">
-          <q-img
-            :src="mainImage.url"
-            :alt="mainImage.altText"
-            :ratio="16 / 9"
-            spinner-color="primary"
-            spinner-size="82px"
-          />
+          <ProductImageViewer :images="produto.imagens" />
         </div>
         <div class="col-xs-12 col-sm-6">
           <h1 class="text-h4 q-mb-sm">{{ produto.nome }}</h1>
@@ -51,6 +45,7 @@ import { useMeta } from 'quasar'
 import { useProductStore } from 'src/stores/product-store'
 import { formatCurrencyFromCents } from 'src/helpers/currency.js'
 import { useTenantStore } from 'src/stores/tenant-store'
+import ProductImageViewer from 'src/components/ProductImageViewer.vue'
 
 export default defineComponent({
   name: 'ProductPage',
@@ -63,6 +58,9 @@ export default defineComponent({
       type: String,
       required: true,
     },
+  },
+  components: {
+    ProductImageViewer,
   },
   preFetch({ store, currentRoute }) {
     const productStore = useProductStore(store)
@@ -83,20 +81,11 @@ export default defineComponent({
         `https://wa.me/${whatsappPedidos.value}?text=Olá, gostaria de saber mais sobre o produto ${productStore.product.nome} [${productStore.product.slug}] ${urlProduto.value}`,
     )
 
-    const mainImage = computed(
-      () =>
-        productStore.product.imagens?.find((image) => image.position === 1) ?? {
-          url: 'https://placedog.net/1024/768',
-          altText: '',
-        },
-    )
-
     return {
       produto: productStore.product,
       whatsappPedidos,
       urlProduto,
       mensagemWhatsapp,
-      mainImage,
       formatCurrencyFromCents,
     }
   },
